a638b19db19ad8a782e06c7003ed792a8000b6d0
javierpf610 <89111637+javierpf610@users.noreply.github.com>
2022-03-18 17:16:42 -0600

    commit

---
+++ pages/test/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-table striped show-empty :items="filtered">
+      <template slot="top-row" slot-scope="{ fields }">
+        <td v-for="field in fields" :key="field.key">
+          <input v-model="filters[field.key]" :placeholder="field.label" />
+        </td>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestPage',
+  layout: 'navFooter',
+  data() {
+    return {
+      filters: {
+        id: '',
+        issuedBy: '',
+        issuedTo: '',
+      },
+      items: [
+        { id: 1234, issuedBy: 'Operator', issuedTo: 'abcd-efgh' },
+        { id: 5678, issuedBy: 'User', issuedTo: 'ijkl-mnop' },
+      ],
+    }
+  },
+  computed: {
+    filtered() {
+      const filtered = this.items.filter((item) => {
+        return Object.keys(this.filters).every((key) =>
+          String(item[key]).includes(this.filters[key])
+        )
+      })
+      return filtered.length > 0
+        ? filtered
+        : [
+            {
+              id: '',
+              issuedBy: '',
+              issuedTo: '',
+            },
+          ]
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
