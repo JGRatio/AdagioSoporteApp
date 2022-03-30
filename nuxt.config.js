7c623d5b1c3f8a8290907e1bfe08b2e6d7d6ed0a
javierpf610 <89111637+javierpf610@users.noreply.github.com>
2022-03-29 18:07:12 -0600

    Functional version

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,9 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'http://localhost:4000/api/v1',
   },
-
+server: {
+    port: 4500,
+},
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
